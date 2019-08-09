e0d5c87e8d5c4c77c769b7ddcd17a9d2981dca7a
yanghakim <yanghakim0@gmail.com>
2019-08-09 12:00:53 -0400

    implemented LINK saving

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -21,6 +21,14 @@ class Section extends Component {
       deletedIndex: null,
       showDeletedNotif: false,
       undoClass: "section-undobtn",
+      linkValue: "",
+      linkPlaceholder: "link to passage",
+      linkBibleText: "",
+      linkDevoText: "",
+      linkBookText: "",
+      linkSermonText: "",
+      linkPoemText: "",
+      linkSongText: "",
       bibleText: [""],
       qtText: [""],
       devoText: [""],
@@ -60,7 +68,8 @@ class Section extends Component {
       switch (prevProps.section) {
         case "bible":
           this.setState({
-            bibleText: this.state.text
+            bibleText: this.state.text,
+            linkBibleText: this.state.linkValue
           });
           break;
         case "quiet times":
@@ -70,17 +79,20 @@ class Section extends Component {
           break;
         case "devotionals/articles":
           this.setState({
-            devoText: this.state.text
+            devoText: this.state.text,
+            linkDevoText: this.state.linkValue
           });
           break;
         case "books/novels":
           this.setState({
-            bookText: this.state.text
+            bookText: this.state.text,
+            linkBookText: this.state.linkValue
           });
           break;
         case "sermons":
           this.setState({
-            sermonText: this.state.text
+            sermonText: this.state.text,
+            linkSermonText: this.state.linkValue
           });
           break;
         case "photography/snapshots":
@@ -90,12 +102,14 @@ class Section extends Component {
           break;
         case "poems":
           this.setState({
-            poemText: this.state.text
+            poemText: this.state.text,
+            linkPoemeText: this.state.linkValue
           });
           break;
         case "songs/lyrics":
           this.setState({
-            songText: this.state.text
+            songText: this.state.text,
+            linkSongText: this.state.linkValue
           });
           break;
         case "meetups":
@@ -117,74 +131,93 @@ class Section extends Component {
       }
 
       this.setState({
-        text: ""
+        text: "",
+        linkValue: ""
       });
 
       switch (this.props.section) {
         case "bible":
           this.setState({
             text: this.state.bibleText,
-            titlePlaceholder: "TITLE"
+            titlePlaceholder: "TITLE",
+            linkPlaceholder: "link to passage",
+            linkValue: this.state.linkBibleText
           });
           break;
         case "quiet times":
           this.setState({
             text: this.state.qtText,
-            titlePlaceholder: "TITLE"
+            titlePlaceholder: "TITLE",
+            linkPlaceholder: null
           });
           break;
         case "devotionals/articles":
           this.setState({
             text: this.state.devoText,
-            titlePlaceholder: "TITLE"
+            titlePlaceholder: "TITLE",
+            linkPlaceholder: "link to devotional/article",
+            linkValue: this.state.linkDevoText
           });
           break;
         case "books/novels":
           this.setState({
             text: this.state.bookText,
-            titlePlaceholder: "TITLE"
+            titlePlaceholder: "TITLE",
+            linkPlaceholder: "link to ebook",
+            linkValue: this.state.linkBookText
           });
           break;
         case "sermons":
           this.setState({
             text: this.state.sermonText,
-            titlePlaceholder: "TITLE"
+            titlePlaceholder: "TITLE",
+            linkPlaceholder: "link to sermon",
+            linkValue: this.state.linkSermonText
           });
           break;
         case "photography/snapshots":
           this.setState({
             text: this.state.photoText,
-            titlePlaceholder: "TITLE"
+            titlePlaceholder: "TITLE",
+            linkPlaceholder: null,
+            linkValue: this.state.linkPhotoText
           });
           break;
         case "poems":
           this.setState({
             text: this.state.poemText,
-            titlePlaceholder: "TITLE"
+            titlePlaceholder: "TITLE",
+            linkPlaceholder: "link to poem",
+            linkValue: this.state.linkPoemText
           });
           break;
         case "songs/lyrics":
           this.setState({
             text: this.state.songText,
-            titlePlaceholder: "TITLE"
+            titlePlaceholder: "TITLE",
+            linkPlaceholder: "link to song",
+            linkValue: this.state.linkSongText
           });
           break;
         case "meetups":
           this.setState({
             text: this.state.meetupText,
-            titlePlaceholder: "NAME"
+            titlePlaceholder: "NAME",
+            linkPlaceholder: null
           });
           break;
         case "relationships":
           this.setState({
             text: this.state.relationshipText,
-            titlePlaceholder: "NAME"
+            titlePlaceholder: "NAME",
+            linkPlaceholder: null
           });
           break;
         case "family":
           this.setState({
             text: this.state.familyText,
-            titlePlaceholder: "NAME"
+            titlePlaceholder: "NAME",
+            linkPlaceholder: null
           });
           break;
         default:
@@ -205,70 +238,6 @@ class Section extends Component {
     const index = e.target.attributes.getNamedItem("index").value;
     this.state.text[index] = e.target.value;
     this.forceUpdate();
-  };
-
-  /**
-   * SETTING header text by section name
-   **/
-  setText = () => {
-    switch (this.props.section) {
-      case "bible":
-        this.setState({
-          bibleText: this.state.text
-        });
-        break;
-      case "quiet times":
-        this.setState({
-          qtText: this.state.text
-        });
-        break;
-      case "devotionals/articles":
-        this.setState({
-          devoText: this.state.text
-        });
-        break;
-      case "books/novels":
-        this.setState({
-          bookText: this.state.text
-        });
-        break;
-      case "sermons":
-        this.setState({
-          sermonText: this.state.text
-        });
-        break;
-      case "photography/snapshots":
-        this.setState({
-          photoText: this.state.text
-        });
-        break;
-      case "poems":
-        this.setState({
-          poemText: this.state.text
-        });
-        break;
-      case "songs/lyrics":
-        this.setState({
-          songText: this.state.text
-        });
-        break;
-      case "meetups":
-        this.setState({
-          meetupText: this.state.text
-        });
-        break;
-      case "relationships":
-        this.setState({
-          relationshipText: this.state.text
-        });
-        break;
-      case "family":
-        this.setState({
-          familyText: this.state.text
-        });
-        break;
-      default:
-    }
   };
 
   /**
@@ -367,6 +336,12 @@ class Section extends Component {
     }, 100);
   };
 
+  handleLinkChange = e => {
+    this.setState({
+      linkValue: e.target.value
+    });
+  };
+
   render() {
     return (
       <div className="section">
@@ -385,10 +360,12 @@ class Section extends Component {
         {this.props.section === "photography/snapshots" && (
           <button className="section-btn">open iCloud photos</button>
         )}
-        {this.props.section === "devotionals/articles" && (
+        {this.state.linkPlaceholder && (
           <input
             className="section-link"
-            placeholder="link to devotional/article"
+            placeholder={this.state.linkPlaceholder}
+            value={this.state.linkValue}
+            onChange={this.handleLinkChange}
           />
         )}
         {this.props.section === "songs/lyrics" && (
